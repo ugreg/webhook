35debed38a779dac320b3499f547a91666f931dc
gregdegruy <gregory.degruy@gmail.com>
2018-10-11 15:12:05 -0700

    + md power platform + testing proper adal implementation

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Webhook
-Example Webhook using REST and JavaScript
+Example Webhook creattion using REST and JavaScript. Powerful API that let's you interact with instances made in the classic admin Dynamics 365 portal. As well as instances made in the new admin center https://admin.powerplatform.microsoft.com/environments that leverage classic Dynamics 365 Solutions, Model and Canvas Driven apps. Tested with v8.2 and v9 instances.
 
 # References
 
-Handle auth using ADAL for NodeJS https://github.com/AzureAD/azure-activedirectory-library-for-nodejs
-Mock an API with https://beeceptor.com/
+- Handle auth using ADAL for NodeJS https://github.com/AzureAD/azure-activedirectory-library-for-nodejs
+- Mock an API with https://beeceptor.com/
 
--- js/src/main.js
+++ js/src/main.js
@@ -1,64 +1,123 @@
 var AuthenticationContext = require('adal-node').AuthenticationContext;
-var http = require('http');
+var crypto = require('crypto');
+var express = require('express');
 var request = require('request');
 
-var math = require('./math').sum;
+var app = express();
 
-var MyApi = MyApi || {};
-
-var clientId = '';
-var clientSecret = '';
+var clientId = process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
 var authorityHostUrl = 'https://login.windows.net';
-var azureAdTenant = 'grdegr.onmicrosoft.com'; 
-var orgUrl = 'https://mssmartsheet.api.crm.dynamics.com';
-// var authUrl = authorityHostUrl + '/' + azureAdTenant + '/oauth2/authorize?resource=' + orgUrl;
-// var accessTokenkUrl = authorityHostUrl + '/' + azureAdTenant + '/oauth2/token?resource=' + orgUrl;
-var callBackUrl = 'http://localhost:1337/getAccessToken';
+var azureAdTenant = 'grdegr.onmicrosoft.com';
+var authorityUrl = authorityHostUrl + '/' + azureAdTenant;
+var redirectUri = 'http://localhost:3000/getAToken';
 
-var proxy = 'http://msssheet.proxy.beeceptor.com';
+var resource = 'https://orgebe09fbc.api.crm.dynamics.com';
+var resourceApiEndpoint = 'api/data/v9.0/';
+var apiUrl = resource + '/' + resourceApiEndpoint;
+var templateAuthzUrl = 'https://login.windows.net/' +
+                        tenant +
+                        '/oauth2/authorize?response_type=code&client_id=' +
+                        clientId +
+                        '&redirect_uri=' +
+                        redirectUri +
+                        '&state=<state>&resource=' +
+                        resource;
 
-var context = new AuthenticationContext(authorityHostUrl + '/' + azureAdTenant);
-resource = orgUrl;
-var accessToken = '';
-
-var acquireTokenWithClientCredentialsHelper = function(resource, clientId, clientSecret) {
-
-  return new Promise(function (resolve, reject) {
-    context.acquireTokenWithClientCredentials(resource, clientId, clientSecret, function(error, tokenResponse) {
-      if (error) {
-        console.log('well that didn\'t work: ' + error.stack);
-        reject();
-      } else {    
-        accessToken = tokenResponse.accessToken;
-        console.log(tokenResponse);
-
-        var options = { 
-          method: 'GET',
-          url: 'https://mssmartsheet.api.crm.dynamics.com/api/data/v9.0/accounts',
-          headers: {
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + accessToken       
-          },
-          json: true 
-        };
-
-        request(options, function (error, response, body) {
-          if (error) { throw new Error(error); }
-
-          console.log(body);
-        });
-
-        resolve();
-      }
-    });  
-  }); 
+function createAuthorizationUrl(state) {
+  return templateAuthzUrl.replace('<state>', state);
 }
 
-acquireTokenWithClientCredentialsHelper(resource, clientId, clientSecret);
+// Clients get redirected here in order to create an OAuth authorize url and redirect them to AAD.
+// There they will authenticate and give their consent to allow this app access to
+// some resource they own.
+app.get('/auth', function(req, res) {
+  crypto.randomBytes(48, function(ex, buf) {
+    var token = buf.toString('base64').replace(/\//g,'_').replace(/\+/g,'-');
+    res.cookie('authstate', token);
+    var authorizationUrl = createAuthorizationUrl(token);
+    console.log('redirecting to auth url: ' + authorizationUrl);
+    res.redirect(authorizationUrl);
+  });
+});
 
-var webhookEndpoint = orgUrl + '/api/data/v9.0/serviceendpoints';
-var payload = 
+// After consent is granted AAD redirects here.  The ADAL library is invoked via the
+// AuthenticationContext and retrieves an access token that can be used to access the
+// user owned resource.
+app.get('/getAToken', function(req, res) {
+
+  var authenticationContext = new AuthenticationContext(authorityUrl);
+
+  console.log('getting auth context');
+
+  authenticationContext.acquireTokenWithAuthorizationCode(
+    req.query.code,
+    redirectUri,
+    resource,
+    clientId,
+    clientSecret,
+    function(err, response) {
+      var message = '';
+      if (err) {
+        message = 'error: ' + err.message + '\n';
+        return res.send(message)
+      }
+      // message += 'response: ' + JSON.stringify(response);
+
+      /***
+       * Response structure will look like this
+       * response: {"tokenType":"Bearer","expiresIn":3599,"expiresOn":"2018-06-26T17:36:16.238Z",
+       * "resource":"https://smartsheetdev.api.crm.dynamics.com","accessToken":"","refreshToken":"","userId":"name@tenant.onmicrosoft.com","isUserIdDisplayable":true,"familyName":"Hedgehog","givenName":"Sonic","oid":"","tenantId":""}
+       */
+      accessToken = response.accessToken;
+
+      res.send('access token updated');
+    }
+  );
+});
+// grab one account and get some fields using $top=1
+app.get('/getaccount', (req, res) => {
+  console.log('in get account');
+
+  var body = '';
+  request({
+    url: apiUrl + '/accounts?$top=1',
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    },
+    json: JSON.stringify(body)
+  }, (err, response, body) => {
+    res.send(response || err);
+
+    if (response) {
+      var account = response.value[0];
+      var accountId = '';
+      var accountName = '';
+
+      var etn = 'account';
+      var pagetype = 'entityrecord';
+      var id = '';
+      var accountLink = '';
+
+      if (account.accountid && account.name) {
+        accountId = account.accountid.toString();
+        accountName = account.name;
+        id = accountId;
+        accountLink = `http://smartsheetdev.crm.dynamics.com/main.aspx?etn=${etn}&pagetype=${pagetype}&id=%7B${id}%7D`;
+      }
+      console.log('GET account with id ' + accountId + ' and Name is ' + accountName);
+      console.log("Here's a link to the account in Dynamics " + accountLink);
+    }
+    else {
+      console.log('response is null');
+    }
+  });
+});
+
+var webhookEndpoint = '' + '/api/data/v9.0/serviceendpoints';
+var proxy = 'http://msbcdemo.proxy.beeceptor.com';
+var payload =
 {
   'messageformat': 2, // json
   'namespaceformat': 1, // Namespace Address
@@ -70,33 +129,4 @@ var payload =
   'name': 'Webhook Using Code',
   'authvalue': 'test'
 }
-// MyApi.registerWebhook = function () {
-
-//   return new Promise(function (resolve, reject) {
-//     MyApi.request('POST', stsTokenRequestUri, 'application/x-www-form-urlencoded', tokenRequestHeaders, tokenRequestParams)
-//     .then(function (response) {
-//       try {
-//         MyApi.configuration.stsToken = response.access_token.toString();
-//         resolve();
-//       } catch (erroror) { reject(new erroror('Failed Promise MyApi.getSpurGoToken' + erroror.message)); }
-//     })
-//     .catch(function(erroror) { reject(new erroror('Failed Promise MyApi.getSpurGoToken' + erroror.message)); });
-//   });  
-// }
-
-// MyApi.request('POST', webhookEndpoint, 'Application-json', null, payload);
-
-// var options = {
-//   host: 'developer.api.autodesk.com',
-//   path: '/oss/v1/buckets',
-//   method: 'POST',
-//   headers: {
-//       'Content-Type': 'application/json'
-//       'Authorization': 'Bearer token'
-//   }
-// };
-//       https://mssmartsheet.api.crm.dynamics.com/api/data/v9.0/accounts
-// var reqBody = JSON.stringify(payload);
-// req.write();
-
 
